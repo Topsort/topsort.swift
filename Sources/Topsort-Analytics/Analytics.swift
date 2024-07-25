35a52ed8ade39ff150e24c67668f3173078ec26b
Sebastian Margozzini <sebastian.margozzini@topsort.com>
2024-07-25 17:58:54 -0400

    feat(analytics): add execute auctions funcitions to analytics sdk

--- Sources/Topsort-Analytics/Analytics.swift
+++ Sources/Topsort-Analytics/Analytics.swift
@@ -5,22 +5,23 @@ public class Analytics {
     @FilePersistedValue(storePath: PathHelper.path(for: "com.topsort.analytics.opaque-user-id.plist"))
     private var _opaqueUserId: String?
     public var opaqueUserId: String {
-        get { 
+        get {
             if let oui = _opaqueUserId {
                 return oui
             } else {
                 let oui = Self.newOpaqueUserId()
                 _opaqueUserId = oui
                 return oui
-            } 
+            }
         }
     }
     private init() {}
     public func set(opaqueUserId: String?) {
         self._opaqueUserId = opaqueUserId ?? Self.newOpaqueUserId()
     }
-    public func configure(apiKey: String, url: String?) {
+    public func configure(apiKey: String, url: String? = nil) {
         EventManager.shared.configure(apiKey: apiKey, url: url)
+        AuctionManager.shared.configure(apiKey: apiKey, url: url)
     }
     public func track(impression event: Event) {
         EventManager.shared.push(event: .impression(event))
@@ -32,4 +33,8 @@ public class Analytics {
         EventManager.shared.push(event: .purchase(event))
     }
     private static func newOpaqueUserId() -> String { UUID().uuidString }
+
+    public func executeAuctions(auctions: [Auction]) async -> AuctionResponse? {
+        return await AuctionManager.shared.executeAuctions(auctions: auctions)
+    }
 }
